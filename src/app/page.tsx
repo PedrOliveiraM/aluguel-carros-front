041b5db7d1e32afa301550eebd8de7e53f5bf173
PedrOliveiraM <pedropucmont@gmail.com>
2024-12-02 15:59:12 -0300

    chore: initialize Next.js project with essential configurations and components

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <div className="flex">
+      <main>
+        <h1 className="text-4xl font-bold">Hello World</h1>
+        <h2>Bem vindo a Locadora de veiculos</h2>
+        <Button>Teste</Button>
+      </main>
+    </div>
+  )
+}
