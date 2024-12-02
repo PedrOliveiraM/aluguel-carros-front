003ab757729b4b2de032b64e19477a812b647028
PedrOliveiraM <pedropucmont@gmail.com>
2024-12-02 15:59:21 -0300

    feat: add Header component and UI elements for Card and Button

---
+++ src/components/Header.tsx
@@ -0,0 +1,44 @@
+import { Menubar, MenubarMenu, MenubarTrigger } from '@/components/ui/menubar'
+import Link from 'next/link'
+
+export async function Header() {
+  return (
+    <Menubar>
+      <MenubarMenu>
+        <MenubarTrigger asChild>
+          <Link href="/">Home</Link>
+        </MenubarTrigger>
+      </MenubarMenu>
+      <MenubarMenu>
+        <MenubarTrigger asChild>
+          <Link href="/clientes">Clientes</Link>
+        </MenubarTrigger>
+      </MenubarMenu>
+      <MenubarMenu>
+        <MenubarTrigger asChild>
+          <Link href="/locacao">Locação</Link>
+        </MenubarTrigger>
+      </MenubarMenu>
+      <MenubarMenu>
+        <MenubarTrigger asChild>
+          <Link href="/devolucao">Devolução</Link>
+        </MenubarTrigger>
+      </MenubarMenu>
+      <MenubarMenu>
+        <MenubarTrigger asChild>
+          <Link href="/veiculos">Veículos</Link>
+        </MenubarTrigger>
+      </MenubarMenu>
+      <MenubarMenu>
+        <MenubarTrigger asChild>
+          <Link href="/manutencao">Manutenção</Link>
+        </MenubarTrigger>
+      </MenubarMenu>
+      <MenubarMenu>
+        <MenubarTrigger asChild>
+          <Link href="/relatorios">Relatórios</Link>
+        </MenubarTrigger>
+      </MenubarMenu>
+    </Menubar>
+  )
+}
